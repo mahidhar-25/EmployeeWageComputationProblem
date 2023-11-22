da29d9662eb0125045c6b64d37db02b2c4bd53b7
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-22 23:40:59 +0530

    Added : usecase 4 , calculating wages using switch

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -3,25 +3,27 @@ public class EmployeeWageProblem {
         System.out.println("!!! Welcome to Employee Wage computation problem !!!");
         int attendance = (int) (Math.random() * 2); // 0 for absent, 1 for present
 
+
         // Displaying the generated attendance status
-        //usecase 1
+        //use case 1
         if (attendance == 1) {
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
         }
-        //usecase 2
+        //use case 2
         int wagePerHour = 20;
         int workingHoursPerDay = 8;
-        if (attendance == 1) {
-            System.out.println("Daily wages per day is : " + wagePerHour * workingHoursPerDay);
-        }
         //use case 3
-        boolean partTime = false;
+        int isPartTime = (int) (Math.random()*2);
+        
         int wagePerHourInPartTime = 8;
-        int noOfHoursWorked = 0;
-        if (partTime == true) {
-            System.out.println("Daily wages for partTime based on working hours : " + wagePerHourInPartTime * noOfHoursWorked);
+        int noOfHoursWorked = 1;
+        //use case 4
+        switch(isPartTime){
+            case 1 -> System.out.println("Daily wages for partTime based on working hours : " + wagePerHourInPartTime * noOfHoursWorked);
+            case 0 -> System.out.println("Daily wages per day is : " + wagePerHour * workingHoursPerDay);
+            default -> System.out.println("he is not an employee");
         }
     }
 }
