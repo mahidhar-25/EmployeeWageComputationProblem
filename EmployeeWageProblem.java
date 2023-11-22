f458420d35ce4f2a12500e15c30248bed32d2e21
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-22 19:33:48 +0530

    Added : usecase 1 to check attendence

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,5 +1,13 @@
 public class EmployeeWageProblem {
     public static void main(String[] args) {
         System.out.println("!!! Welcome to Employee Wage computation problem !!!");
+        int attendance = (int) (Math.random() * 2); // 0 for absent, 1 for present
+
+        // Displaying the generated attendance status
+        if (attendance == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
