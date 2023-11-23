3cccad3110fccf9c217b2306f934337cd83ffa23
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-23 08:38:05 +0530

    Added : comments were added to the employee wage problem

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,15 +1,32 @@
+
+/*
+@desc :
+1. first welcome the employee by displaying message
+2. finding employee is part-time or full time by random
+3.calculating the attendance for the full month based on the given condition
+4. displaying the wages based for daily and monthly based on the type of job
+ */
 public class EmployeeWageProblem {
     public static void main(String[] args) {
         System.out.println("!!! Welcome to Employee Wage computation problem !!!");
 
         //use case 3
+        /*
+        checking whether employee is part-time or full time
+         */
         int isPartTime = (int) (Math.random()*2);
 
+        /*
+        Printing out the employee position
+         */
         if (isPartTime == 1) {
             System.out.println("Employee is Part Time");
         } else {
             System.out.println("Employee is Full Time");
         }
+        /*
+        Declaring all the variables
+         */
         int attendance;
         int NoOfHoursWorkedPartTime;
         int totalWorkingDays = 0;
@@ -17,6 +34,10 @@ public class EmployeeWageProblem {
         int workingHoursPerDay = 8;
         int wagePerHour = 20;
         int wagePerHourPartTime = 8;
+
+        /*
+        generating every-days attendance , if attended how many hours he worked based on the type of job
+         */
         for(int i=0;i<=31;i++){
             if(totalWorkingHours >= 100 ){
                 System.out.println("Total working hours has crossed 100");
@@ -50,7 +71,7 @@ public class EmployeeWageProblem {
         if(totalWorkingHours <100 && totalWorkingDays<20){
             System.out.println("Month has completed");
         }
-//use case 2 ,4 , 5 ,  6
+      //displaying daily , monthly wages based on the type of job
         switch(isPartTime){
             case 1 -> {
                 System.out.println("\nMonthly wages for partTime working, total working hours are " + totalWorkingHours + "\n Monthly wage is : " + totalWorkingHours*wagePerHourPartTime);
