8f71732cbd07d5ec27aefd1a1d93f5b39378a2d7
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-26 17:33:43 +0530

    Added : usecase 8 , computed employee wages for multiple companies by creating an organization class

--- com/bridgelabz/EmployeeWage/EmployeeWageApplication.java
+++ com/bridgelabz/EmployeeWage/EmployeeWageApplication.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
  */
 public class EmployeeWageApplication {
 
-    public static final int CREATE_NEW_COMPANY = 1;
+    public static final int CREATE_NEW_ORGANIZATION = 0;
+    public static final int ADD_NEW_COMPANY = 1;
     public static final int ADD_NEW_EMPLOYEE = 2 ;
     public static final int REMOVE_OLD_EMPLOYEE = 3;
     public static final int CALCULATE_WAGE_OF_EMPLOYEE = 5;
@@ -15,13 +16,14 @@ public class EmployeeWageApplication {
     public static final int DISPLAY_WAGES = 7;
     public static final int DISPLAY_COMPANY = 8;
     public static final int DISPLAY_EMPLOYEE = 6;
+    public static final int DISPLAY_ORGANIZATION = 9;
     public static final int EXIT = 10;
 
 
     public static void main(String[] args) {
 
-
-        System.out.println("1. To initialize a  company");
+        System.out.println("1. To initialize a  organization");
+        System.out.println("1. To add new Company to existing organization");
         System.out.println("2. To add new Employee to existing company");
         System.out.println("3. To remove Employee from existing company");
         System.out.println("4. To Generate Random Attendance of Employee");
@@ -29,74 +31,119 @@ public class EmployeeWageApplication {
         System.out.println("6. To display employee details");
         System.out.println("7. To display wages for the respective company");
         System.out.println("8. To display company details");
+        System.out.println("8. To display organization details");
         System.out.println("10 . To exit");
         System.out.println("choose an option from the above every time : ");
         Scanner input = new Scanner(System.in);
         int option = input.nextInt();
         Company company = null;
+        Organizations organization = null;
         Employee employee = null;
         String companyName , employeeId;
         while(option != EXIT){
             switch(option){
-                case CREATE_NEW_COMPANY -> {
-                    company = Company.getCompanyDetailsFromUser();
-                    System.out.println("Company has created");
+                case CREATE_NEW_ORGANIZATION -> {
+                    organization = Organizations.getOrganizationDetailsFromUser();
+                    System.out.println("Organization has created");
+                }
+                case ADD_NEW_COMPANY -> {
+                    if(organization != null) {
+                        company = Company.getCompanyDetailsFromUser();
+                        organization.addCompanyToOrganization(company);
+                        System.out.println("Company has added");
+                    }
                 }
                 case ADD_NEW_EMPLOYEE -> {
-                    if(company != null) {
+                    if(organization != null) {
                         System.out.println("Enter  company name  is : ");
                         companyName = input.next();
-                        employee = Employee.getEmployeeDetailsFromUser();
-                        company.addEmployeeToCompany(employee);
+                        company = organization.getCompanyDetailsFromOrganizations(companyName);
+                        if (company != null) {
+                            employee = Employee.getEmployeeDetailsFromUser();
+                            company.addEmployeeToCompany(employee);
+                        }
                     }
                 }
                 case REMOVE_OLD_EMPLOYEE -> {
-                    if (company != null) {
+                    if(organization != null) {
                         System.out.println("Enter  company name  is : ");
                         companyName = input.next();
-                        System.out.println("Enter employee id is : ");
-                        employeeId = input.next();
-                        company.removeEmployeeFromCompany(employeeId);
+                        company = organization.getCompanyDetailsFromOrganizations(companyName);
+                        if (company != null) {
+                            System.out.println("Enter employee id is : ");
+                            employeeId = input.next();
+                            company.removeEmployeeFromCompany(employeeId);
+                        }
                     }
                 }
                 case GENERATE_ATTENDANCE -> {
-                    if(company != null) {
-                        System.out.println("Enter employee id is : ");
-                        employeeId = input.next();
-                        employee = company.getEmployeeDetailsFromCompany(employeeId);
-                        if (employee != null) {
-                            company.generateRandomAttendance(employee);
+                    if(organization != null) {
+                        System.out.println("Enter  company name  is : ");
+                        companyName = input.next();
+                        company = organization.getCompanyDetailsFromOrganizations(companyName);
+                        if (company != null) {
+                            System.out.println("Enter employee id is : ");
+                            employeeId = input.next();
+                            employee = company.getEmployeeDetailsFromCompany(employeeId);
+                            if (employee != null) {
+                                company.generateRandomAttendance(employee);
+                            }
                         }
                     }
                 }
                 case CALCULATE_WAGE_OF_EMPLOYEE -> {
-                    if(company != null) {
-                        System.out.println("Enter employee id is : ");
-                        employeeId = input.next();
-                        employee = company.getEmployeeDetailsFromCompany(employeeId);
-                        if (employee != null) {
-                            System.out.println("total wage of employee is " + company.getEmployeeWage(employee));
+                    if(organization != null) {
+                        System.out.println("Enter  company name  is : ");
+                        companyName = input.next();
+                        company = organization.getCompanyDetailsFromOrganizations(companyName);
+                        if (company != null) {
+                            System.out.println("Enter employee id is : ");
+                            employeeId = input.next();
+                            employee = company.getEmployeeDetailsFromCompany(employeeId);
+                            if (employee != null) {
+                                System.out.println("total wage of employee is " + company.getEmployeeWage(employee));
+                            }
                         }
                     }
                 }
                 case DISPLAY_EMPLOYEE -> {
-                    if(company != null) {
-                        System.out.println("Enter employee id is : ");
-                        employeeId = input.next();
-                        employee = company.getEmployeeDetailsFromCompany(employeeId);
-                        if (employee != null) {
-                            System.out.println("Employee details are : \n" + employee);
+                    if(organization != null) {
+                        System.out.println("Enter  company name  is : ");
+                        companyName = input.next();
+                        company = organization.getCompanyDetailsFromOrganizations(companyName);
+                        if (company != null) {
+                            System.out.println("Enter employee id is : ");
+                            employeeId = input.next();
+                            employee = company.getEmployeeDetailsFromCompany(employeeId);
+                            if (employee != null) {
+                                System.out.println("Employee details are : \n" + employee);
+                            }
                         }
                     }
                 }
                 case DISPLAY_WAGES -> {
-                    if(company != null) {
-                        System.out.println("Wages details are : \n" + company.getWages());
+                    if(organization != null) {
+                        System.out.println("Enter  company name  is : ");
+                        companyName = input.next();
+                        company = organization.getCompanyDetailsFromOrganizations(companyName);
+                        if (company != null) {
+                            System.out.println("Wages details are : \n" + company.getWages());
+                        }
                     }
                 }
                 case DISPLAY_COMPANY -> {
-                    if(company != null) {
-                        System.out.println("company details are : \n" + company);
+                    if(organization != null) {
+                        System.out.println("Enter  company name  is : ");
+                        companyName = input.next();
+                        company = organization.getCompanyDetailsFromOrganizations(companyName);
+                        if (company != null) {
+                            System.out.println("company details are : \n" + company);
+                        }
+                    }
+                }
+                case DISPLAY_ORGANIZATION -> {
+                    if(organization != null) {
+                        System.out.println("company details are : \n" + organization);
                     }
                 }
                 default -> System.out.println("choose the correct option from the above");
---
+++ com/bridgelabz/EmployeeWage/Organizations.java
@@ -0,0 +1,110 @@
+package com.bridgelabz.EmployeeWage;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+@desc : Organization class hold a set of companies and with unique organization id and set of methods to take user input
+   and add , remove a company from organization.
+ */
+public class Organizations {
+    ArrayList<Company> companies;
+    private String organizationId;
+
+    /*
+@desc : this function takes the series of inputs required to create an instance of organization from user
+@params : no params
+@return : return an organization instance by initializing
+     */
+
+    public static Organizations getOrganizationDetailsFromUser(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter organization id : ");
+        String organizationId = input.next();
+        return new Organizations(organizationId);
+    }
+
+    /*
+@desc : constructor initializes the organization instance , and companies list
+@params - string : organization id
+@return : no return
+ */
+    public Organizations(String organizationId) {
+        this.organizationId = organizationId;
+        companies = new ArrayList<>();
+    }
+
+    /*
+    @desc : new company will added to the collect of companies inside a organization
+    @params -  Company : new compnay with initializing
+    @return : no return
+     */
+    public void addCompanyToOrganization(Company newCompany){
+        companies.add(newCompany);
+    }
+
+    /*
+@desc :  this function will find out the company by taking company name
+         as a param in the collection of company inside a organization
+@params - String : company Name
+@return - Company - returns an matching instance of company otherwise return a null
+
+ */
+    public Company getCompanyDetailsFromOrganizations(String companyName){
+        for(Company organization : companies){
+            if(organization.getCompanyName().equals(companyName)){
+                return organization;
+            }
+        }
+        return null;
+    }
+
+    /*
+@desc :  this function will remove the company  by taking company name
+        as a param in the collection of company  inside a organization
+@params -String : company Name
+@return - void - no return
+
+*/
+    public void removeCompanyFromOrganization(String companyName){
+        for(Company organization : companies){
+            if(organization.getCompanyName().equals(companyName)){
+                companies.remove(organization);
+            }
+        }
+    }
+
+    /*
+@desc : it gives the organizationId
+@param : no params
+@return - String : organizationId
+ */
+
+    public String getSoftwareOrganizationId() {
+        return organizationId;
+    }
+
+        /*
+@desc : it sets/updates the organization id
+@param : no params
+@return - String : organizationId
+ */
+
+
+    public void setSoftwareOrganizationId(String softwareOrganizationId) {
+        this.organizationId = softwareOrganizationId;
+    }
+    /*
+@desc :Returns a string representation of the organization object.
+@params : no params
+@return A string containing the formatted details of the organization.
+*/
+    @Override
+    public String toString() {
+        return "Organizations : \n" +
+                "{" +
+                "   softwareOrganizationId = " + organizationId + "\n" +
+                "   companies = " + companies + "\n" +
+                '}';
+    }
+}
